4e1dfd9759e3b15275cb4fc4420b92acf7189806
goosman.lei <goosman.lei@gmail.com>
2022-03-14 16:23:29 +0800

    upgrade. random format

--- ex.002/greetings/greetings.go
+++ ex.002/greetings/greetings.go
@@ -3,11 +3,27 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	return fmt.Sprintf("Hi, %v. Welcome!", name), nil
+	return fmt.Sprintf(randomFormat(), name), nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
